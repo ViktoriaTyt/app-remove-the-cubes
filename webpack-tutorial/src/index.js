aae5fbd9a3c58862e08b7b89f60fd7358af949e5
Vika Beletskaya <victoriab@wizardsdev.com>
2020-12-12 16:52:42 +0200

    add classname to color

--- webpack-tutorial/src/index.js
+++ webpack-tutorial/src/index.js
@@ -9,21 +9,60 @@ class Square {
     newDiv = document.createElement("div");
 
     initSquare() {
-        this.newDiv.style.backgroundColor = this.randomColor;
+        this.newDiv.style.backgroundColor = this.randomColor[1];
         this.newDiv.style.top = this.randomPositionTop;
-        this.newDiv.style.left = this.randomPositionLeft;
-        this.newDiv.classList.add('square-item')
-        console.log(this.newDiv)
+        this.newDiv.style.left = this.randomPositionLeft;;
+        this.newDiv.classList.add('square-item');
+        this.newDiv.classList.add(this.randomColor[0]);
+
         return this.newDiv;
     }
 }
+
 const gameField = document.querySelector('.main-field');
-const colors = ['red', 'blue', 'green'];
-const randomColor = colors.[Math.floor(Math.random() * colors.length)];
+
+const randomColor = () =>{
+    const colorsClass = [['redItem', 'red'],['blueItem', 'blue'],['greenItem', 'green']];
+    let number = (Math.floor(Math.random() * 3));
+    let arr=[];
+    let choosenItem;
+    colorsClass.forEach(function (item,index) {
+        arr.push(item)
+    })
+    return choosenItem = arr[number];
+};
+
+const randomQuantity = () => (Math.floor(Math.random() * 3) + 1);
+const randomPositionLeft = () => (Math.floor(Math.random() * gameField.offsetWidth )) + 'px';
+const randomPositionTop = () =>(Math.floor(Math.random() * gameField.offsetHeight )) + 'px';
+
+const addAnotherSquare = () => gameField.append(new Square(randomPositionLeft(), randomPositionTop(),randomColor()).initSquare());
+addAnotherSquare()
+
+const removeItemSquare = (square) => square.parentNode.removeChild(square);
+//const addAnotherSquare = (square) =>
+/*class RandomQuantity extends Square {
+    randomQuantity(){
+        alert(this.randomColor + 'прячется!');
+    }
+}
+const quantitySquare = new RandomQuantity(randomPositionLeft(), randomPositionTop(),randomColor()).initSquare();
+quantitySquare.initSquare();*/
 
 
-const randomPositionLeft = (Math.floor(Math.random() * gameField.offsetWidth )) + 'px';
-const randomPositionTop = (Math.floor(Math.random() * gameField.offsetHeight )) + 'px';
+gameField.addEventListener('click', (e) => {
+    console.log(e.target)
+    if(e.target.classList.contains === 'square-item'){
+        console.log('qwe')
+       removeItemSquare(e.target);
+        let i = 0;
+        const randomQuantityNum = randomQuantity();
+        while( i < randomQuantityNum){
+            i++;
+            addAnotherSquare()
+        }
+    }
+});
 
-gameField.append(new Square(randomPositionLeft, randomPositionTop,randomColor).initSquare())
+
 
